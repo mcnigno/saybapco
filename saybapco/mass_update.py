8b2ffd1a6e3a99c0ea52ebf9e0a8ab18696df946
Mc Nigno <mcnigno@gmail.com>
2018-05-26 21:40:14 +0200

    added mass_update.py

---
+++ saybapco/mass_update.py
@@ -0,0 +1,210 @@
+import glob, os
+from app import models, db
+from flask_appbuilder import filemanager
+import uuid, openpyxl
+from helpers import sanetext
+errors_list = set()
+
+def comments(item):
+
+    print('file processed:', str(item.file))
+    file = open('/Users/danilo/python/saybapco/saybapco/CS_OLD/' + str(item.file), mode='rb')
+    #print('file processed:', file)
+
+    #file = open('/Users/dp/py3/saybapco/saybapco/comments/019-C-GAD-10010-001-RAP-CS REPLY.xlsx', mode='rb')
+    #filename = '019-C-GAD-10010-001-RAP-CS REPLY.xlsx'
+
+    filename = str(item.file)
+    unit = filename[0:3]
+
+    #partner = filename[4]
+
+    mat = filename[4]
+    doctype = filename[6:9]
+    serial = filename[10:15]
+    type_reply = False
+
+    try:
+        #if filename[26:29] == "REP":
+        if filename[-8:-5] == "REP":
+            print('Reply identification: ', filename[-8:-5] )
+            type_reply = True
+    except:
+        pass
+    
+    wb = openpyxl.load_workbook(file)
+    ws = wb.active
+
+    session = db.session
+    comments= models.Comments
+    documents = models.Document
+    revisions = models.Revisions
+
+    revision = session.query(revisions).filter(revisions.revision == item.revision, revisions.document_id == item.document_id).first()
+    #revision = session.query(revisions).filter(revisions.id == item.id).first()
+    session.query(comments).filter(comments.document_id == item.document_id, comments.revision_id == revision.id).delete()
+    
+    try:
+
+        print('doc id',item.document_id,'rev id', revision.id, 'item id', item.id)
+        
+        comm_list = session.query(comments).filter(comments.document_id == item.document_id, comments.revision_id == revision.id).all()
+        print('the comments len is:', len(comm_list))
+        
+        session.query(comments).filter(comments.document_id == item.document_id, comments.revision_id == revision.id).delete()
+        print('delete query executed')
+    
+    except:
+
+        print('some problem here')
+        pass
+    
+
+    #session.commit()
+    #check columns label
+    #for row in ws.iter_colum()
+    #print('document id', item.document_id,'revision:', item.id)
+    partner = item.partner
+    #revision = item.revision
+    
+    for row in ws.iter_rows(min_row=2):
+        
+        if row[0].value is not None:
+            #print(row[0].value, row[1].value, row[2].value,
+              #row[3].value, row[4].value, row[5].value, row[6].value, row[7].value)
+            try:
+
+
+                id_c = sanetext(row[0].value)
+                style = sanetext(row[1].value)
+                page = sanetext(row[2].value)
+                author = sanetext(row[3].value)
+                comment = sanetext(row[4].value)
+                reply = sanetext(row[5].value)
+                included = sanetext(row[6].value)
+                closed = sanetext(row[7].value)
+
+
+                print(id_c, style, author)
+                
+
+
+                if closed == 'Y':
+                    closed = True
+                else:
+                    closed = False
+                
+                if included == 'Y':
+                    included = True
+                else:
+                    included = False
+                                
+                print('before comment')
+                print('document id', item.document_id,'revision:', item.id)
+                comm = comments(created_by_fk = 1, changed_by_fk = 1, id_c=id_c, partner=partner, style=style, page=page, author=author, comment=comment,
+                                reply=reply, included=included, closed=closed,
+                                document_id=item.document_id, revision_id=item.id, type_reply=type_reply)
+                
+                session.add(comm)
+
+                print('after comment')
+            
+                
+            except:
+                print('something is None')
+                errors_list.add(str(item.file))
+                pass
+    
+    session.commit()
+    print('list of Errors:')
+    print(errors_list)
+    return 'done'
+
+
+def check_Doc(item):
+    filename = item
+    unit = filename[0:3]
+    partner = filename[4:7]
+    mat = filename[4]
+    doctype = filename[6:9]
+    serial = filename[10:15]
+    sheet = filename[16:19]
+
+    
+    session = db.session
+    doc = models.Document
+    document = session.query(doc).filter(doc.unit == unit, doc.materialclass == mat,
+                                 doc.doctype == doctype, doc.serial == serial, doc.sheet == sheet).first()
+
+    if document:
+        return document.id, document.partner
+    else:
+        document = doc(created_by_fk = 1, changed_by_fk = 1, unit=unit, materialclass=mat, doctype=doctype, 
+                       serial=serial, sheet=sheet, partner=partner)
+        
+        session.add(document)
+
+        session.flush()
+        session.commit()
+
+        return document.id, document.partner
+
+
+def mass_update():
+    os.chdir('CS_OLD')
+
+    rev_order = ['A','B','C','D','E','F','G','H','I','L','M','N','O','P','Q','R','S','T',
+                'U','V','Z','0','1','2','3','4','5','6','7','8','9','10']
+
+    for revision in rev_order:
+        print('processing Rev:', revision)
+        # Check the revision A ...
+        for file in glob.glob("*.xlsx"):
+            
+            _, _, _, _, _, rev, cs = file.split('-')
+            cs = cs[:-5]
+            #print('rev:', rev,'cs:', cs)
+            
+            if rev[1] == revision and cs != 'CS REP':
+                file_sec = filemanager.secure_filename(file)
+                print(file_sec)
+                file_byte = open(file, mode='rb') 
+                #filemanager.FileManager.save_file(file_byte, file_sec)
+                new_rev = models.Revisions(created_by_fk = 1, changed_by_fk = 1, file=file_sec, revision = rev[1:], trasmittal = 'to update', date_trs = '2015-01-01')
+                new_rev.document_id, new_rev.partner = check_Doc(file) 
+                #new_rev.revision = revision
+                
+                db.session.add(new_rev)
+
+
+                db.session.flush()
+                comments(new_rev)
+
+                print(file)
+        db.session.commit()
+        # Check for the replay
+                
+        for file in glob.glob("*.xlsx"):
+            _, _, _, _, _, rev, cs = file.split('-')
+            cs = cs[:-5]
+            if rev[1] == revision and cs == 'CS REP':
+                file_sec = file
+                print(file_sec)
+                file_byte = open(file, mode='rb') 
+                #filemanager.FileManager.save_file(file_byte, file_sec)
+                new_rev = models.Revisions(created_by_fk = 1, changed_by_fk = 1, file=file_sec, revision = rev[1:], trasmittal = 'to update', date_trs = '2015-01-01')
+                new_rev.document_id, new_rev.partner = check_Doc(file) 
+                new_rev.revision = revision
+                new_rev.reply = True
+
+                db.session.add(new_rev)
+
+
+                db.session.flush()
+                comments(new_rev)
+
+                print(file)
+        db.session.commit()
+
+
+
