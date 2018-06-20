44c83281d04c2846221c46c34715434369c037e5
Mc Nigno <mcnigno@gmail.com>
2018-06-20 11:22:54 +0200

    fixed set_comments blank and close included

--- saybapco/app/views.py
+++ saybapco/app/views.py
@@ -262,7 +262,7 @@ appbuilder.add_view(CommentsPieChart,'Comment Pie Chart',icon="fas fa-code-branc
 appbuilder.add_view_no_menu(Report)
 
 #mass_update()
-#set_comments_blank()
+set_comments_blank()
 #set_comments_included()
 #transmittall()
 
