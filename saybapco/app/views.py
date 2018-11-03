6e38cb5ed6eec59f8b4581d2be30bf8e955af3a8
Mc Nigno <mcnigno@gmail.com>
2018-11-03 18:15:24 +0100

    fix

--- saybapco/app/views.py
+++ saybapco/app/views.py
@@ -899,7 +899,7 @@ appbuilder.add_view_no_menu(CurrentCommentView)
 #appbuilder.add_view(CommentsChart,'Comment Chart',icon="fas fa-code-branch", category="Statistics", category_icon='fas fa-comment')
 #appbuilder.add_view(CommentsPieChart,'Comment Pie Chart',icon="fas fa-code-branch", category="Statistics", category_icon='fas fa-comment')
 
-set_position()   
+#set_position()   
 #mass_update()  
 #set_comments_blank()
 #set_comments_included()
