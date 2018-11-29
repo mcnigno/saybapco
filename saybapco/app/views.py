aef4bd1985769a201389f0450bb1ca2ecc9c2f78
Mc Nigno <mcnigno@gmail.com>
2018-11-29 07:02:57 +0100

    document chart active

--- saybapco/app/views.py
+++ saybapco/app/views.py
@@ -807,7 +807,7 @@ class DocumentView(ModelView):
     
     #list_template = 'appbuilder/general/model/multiple_views.html'
     show_template = 'appbuilder/general/model/show_cascade.html'
-    #show_widget = ShowDocument
+    show_widget = ShowDocument
     
 
     add_exclude_columns = ['created_on', 'changed_on','comments']
