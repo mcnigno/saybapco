87f75ea572be6f79ebd51640cc9a51c3a1a30c35
Mc Nigno <mcnigno@gmail.com>
2019-01-13 19:18:19 +0100

    x

--- saybapco/app/static/js/superset.js
+++ saybapco/app/static/js/superset.js
@@ -1,7 +1,4 @@
 console.log('superset js ready')
-$('#DataTables_Table_4 > tbody > tr > td > span').each(function(span) {
-    console.log(span)
-});
 $(document).ready(function(){
     $("#anchorID").click(function(){
         console.log('jojoj')
