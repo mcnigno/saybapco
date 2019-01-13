dbb085bb9ca4e38210e7c176cab0cbbf0bec4696
Mc Nigno <mcnigno@gmail.com>
2019-01-13 19:01:10 +0100

    fix superset js

--- saybapco/app/static/js/superset.js
+++ saybapco/app/static/js/superset.js
@@ -2,3 +2,8 @@ console.log('superset js ready')
 $('#DataTables_Table_4 > tbody > tr > td > span').each(function(span) {
     console.log(span)
 });
+document.getElementById("anchorID").click(function(){
+    console.log('jojoj')
+}
+    
+);
